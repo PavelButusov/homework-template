85a300fa80d2fbf84e4d4d395cafbd9f30b661b9
PavelButusov <mitialuneff@yandex.ru>
2017-11-02 17:41:46 +0300

    1

--- homework7/decorators.py
+++ homework7/decorators.py
@@ -1,11 +1,29 @@
 
 def cancel(func):
-    pass
+    def cancel_func():
+        return IndexError(func.__name__,'is canceled!')
+    return cancel_func
 
 
 def count_execution(func):
-    pass
+    import time
+
+    def wrappers():
+        t = time.clock()
+        return func.__name__, time.clock() - t
+        yield wrappers
+    def wrapper():
+        wrapper.called += 1
+        return func()
+
+    wrapper.called = 0
+    wrapper.__name__ = func.__name__
+    return wrapper
 
 
 def catch(func):
-    pass
+    def wrapper():
+        try:
+            print(func())
+        except Exception as e:
+            print(e)
